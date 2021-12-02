2314a61af0d1e0ecab1378699b63defb5edfdc32
gopalbbharadva <gopalbbharadva@gmail.com>
2021-12-02 06:00:58 +0530

    fix:object type error

--- src/Contexts/Themecontext.js
+++ src/Contexts/Themecontext.js
@@ -1,5 +1,4 @@
-import React, { createContext } from "react";
-import { useState } from "react/cjs/react.development";
+import React, { createContext, useState } from "react";
 
 export const themeContext = createContext();
 
